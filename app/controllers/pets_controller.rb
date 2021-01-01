23226c4d9fb9d06e6dee5a0d8f2f965511e7fd07
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-01 15:48:53 -0600

    commit tree 8218f717dc7bf136dc3122350e7a94fe380f0947

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,18 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
 
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,7 +26,21 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+
   patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+
+    @pet.save
+
 
     redirect to "pets/#{@pet.id}"
   end
